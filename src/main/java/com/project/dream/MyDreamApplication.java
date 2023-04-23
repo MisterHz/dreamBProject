707826aa45a08d8af7442ed282de2731d5e0737a
fiest fast <fiest.fast+github@gmail.com>
2023-04-23 21:42:33 +0700

    update version java

--- src/main/java/com/project/dream/MyDreamApplication.java
+++ src/main/java/com/project/dream/MyDreamApplication.java
@@ -4,10 +4,18 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyDreamApplication {
 
+	@GetMapping("/message")
+	public String massage(){
+		return "Congrats successfully in Azure !";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MyDreamApplication.class, args);
 		// SpringApplication app = new SpringApplication(MyDreamApplication.class);
